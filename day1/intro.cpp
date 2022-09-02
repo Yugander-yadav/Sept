299ceb0d31a67749652bf4f2d0da3b7f595a0cff
yugander <yuganderpilli@outlook.com>
2022-09-02 18:37:15 +0530

    day 2 progress

---
+++ day1/intro.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "sample.h"
+using namespace std;
+int main(){
+    cout<<"placement lagegi"<<endl;
+    cout<<a;
+//  Data types in cpp 
+
+do{
+cout<<"hello world";
+}
+while(1);
+
+
+
+  
+
+}
